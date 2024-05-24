af4399e1b2f436714f30278d4eb697ce2ad77970
Mark Rudolph <mark@k8ty.app>
2024-05-24 16:33:33 -0400

    zeroth commit

---
+++ X10Toolkit.scala
@@ -0,0 +1,14 @@
+//> using scala 3
+//> using publish.organization  dev.alteration.x10
+
+//> using dep dev.zio::zio::2.1.1
+//> using dep dev.zio::zio-streams::2.1.1
+
+//> using dep com.softwaremill.sttp.tapir::tapir-core::1.10.7
+//> using dep com.softwaremill.sttp.tapir::tapir-sttp-client::1.10.7
+//> using dep com.softwaremill.sttp.tapir::tapir-json-upickle::1.10.7
+
+//> using dep com.lihaoyi::upickle::3.3.1
+//> using dep com.lihaoyi::pprint::0.9.0
+//> using dep com.lihaoyi::os-lib::0.10.1
+//> using dep com.lihaoyi::fansi::0.5.0
---
+++ publish-conf.scala
@@ -0,0 +1,8 @@
+//> using publish.name toolkit
+//> using publish.computeVersion git:tag
+//> using publish.url https://github.com/alterationx10/x10-toolkit
+//> using publish.vcs github:alterationx10/x10-toolkit
+//> using publish.license Apache-2.0
+//> using publish.repository central
+//> using publish.developer "alterationx10|Mark Rudolph|https://github.com/alterationx10"
+//> using repository sonatype:public
